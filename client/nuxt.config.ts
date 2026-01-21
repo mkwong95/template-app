620b7ba799b2b9dfabbcc55aa1b099d752ff06d1
mkwong95 <mk.wong@aol.com>
2026-01-21 18:08:42 +0800

    Install Nuxt Sidebase Auth

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n']
-})
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@sidebase/nuxt-auth'],
+    runtimeConfig: {
+        public: {
+            appUrl: process.env.APP_URL,
+        },
+    },
+    css: ['~/assets/css/fonts.css', '~/assets/css/icons.css'],
+    app: {
+        cdnURL: `${process.env.APP_URL}/client/${process.env.APP_ENV}/`,
+    },
+    nitro: {
+        output: {
+            publicDir: `../public/client/${process.env.APP_ENV}`,
+        },
+    },
+});
